d0f31efca02f78aabc1a68ad5f7a3a4985543c4a
Mohamed Hedeya <mohamedhadia611@gmail.com>
2024-01-02 23:02:40 +0200

    style(qa): override  mantine styles for inputs and modal

--- theme.ts
+++ theme.ts
@@ -3,5 +3,12 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  defaultRadius: "xs",
+  components: {
+    Modal: {
+      styles: {
+        content: { backgroundColor: "#252525", color: "#BABABA" },
+      },
+    },
+  },
 });
